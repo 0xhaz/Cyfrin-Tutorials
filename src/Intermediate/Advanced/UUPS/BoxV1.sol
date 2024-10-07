6348f2f070f161bece1b0d447dd429a0f333441c
0xhaz <28639279+0xhaz@users.noreply.github.com>
2024-10-08 01:19:11 +0800

    Final UUPS

---
+++ src/Intermediate/Advanced/UUPS/BoxV1.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.25;
+
+import { ERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import { ERC20PermitUpgradeable } from
+    "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+// storage is stored in the proxy, NOT in the implementation contract
+// Proxy(number = 0) -> Implementation(number = 1)
+
+contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    uint256 internal number;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function intialize() public initializer {
+        __Ownable_init(msg.sender); // set owner to msg.sender
+        __UUPSUpgradeable_init();
+    }
+
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+
+    function version() external pure returns (uint256) {
+        return 1;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override { }
+}
