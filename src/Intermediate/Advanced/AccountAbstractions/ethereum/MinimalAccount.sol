d1bbc5c57a3c78235d9df649ace8f14a4c18e435
0xhaz <28639279+0xhaz@users.noreply.github.com>
2024-10-11 06:33:43 +0800

    Minimal Account

--- src/Intermediate/Advanced/AccountAbstractions/ethereum/MinimalAccount.sol
+++ src/Intermediate/Advanced/AccountAbstractions/ethereum/MinimalAccount.sol
@@ -1,4 +1,35 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.25;
 
-contract MinimalAccount { }
+import { IAccount } from "account-abstraction/interfaces/IAccount.sol";
+import { PackedUserOperation } from "account-abstraction/interfaces/PackedUserOperation.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract MinimalAccount is IAccount, Ownable {
+    constructor() Ownable(msg.sender) { }
+
+    // A signature is valid, if it's the MinimalAccount owner
+    function validateUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    )
+        external
+        returns (uint256 validationData)
+    {
+        _validateSignature(userOp, userOpHash);
+    }
+
+    function _validateSignature(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash
+    )
+        internal
+        view
+        returns (uint256 validationData)
+    {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+    }
+}
