2ccd4e7c62252d85fdbe32a456d4bda0035400d6
0xhaz <28639279+0xhaz@users.noreply.github.com>
2024-10-12 06:12:10 +0800

    Added Script

---
+++ script/DeployMinimal.s.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.25;
+
+import { Script, console } from "forge-std/Script.sol";
+import { MinimalAccount } from "src/Intermediate/Advanced/AccountAbstractions/ethereum/MinimalAccount.sol";
+
+contract DeployMinimal is Script { }
--- src/Intermediate/Advanced/AccountAbstractions/ethereum/MinimalAccount.sol
+++ src/Intermediate/Advanced/AccountAbstractions/ethereum/MinimalAccount.sol
@@ -6,9 +6,55 @@ import { PackedUserOperation } from "account-abstraction/interfaces/PackedUserOp
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import { SIG_VALIDATION_SUCCESS, SIG_VALIDATION_FAILED } from "account-abstraction/core/Helpers.sol";
+import { IEntryPoint } from "account-abstraction/interfaces/IEntryPoint.sol";
 
 contract MinimalAccount is IAccount, Ownable {
-    constructor() Ownable(msg.sender) { }
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+    error NonceMismatch();
+    error MinimalAccount__NotFromEntryPoint();
+    error MinimalAccount__NotFromEntryPointOrOwner();
+    error MinimalAccount__CallFailed(bytes result);
+
+    /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+    IEntryPoint private immutable i_entryPoint;
+    uint256 ourNonce = 0;
+
+    /*//////////////////////////////////////////////////////////////
+                               MODIFIERS
+    //////////////////////////////////////////////////////////////*/
+    modifier requireFromEntryPoint() {
+        if (msg.sender != address(i_entryPoint)) revert MinimalAccount__NotFromEntryPoint();
+        _;
+    }
+
+    modifier requireFromEntryPointOrOwner() {
+        if (msg.sender != address(i_entryPoint) && msg.sender != owner()) {
+            revert MinimalAccount__NotFromEntryPointOrOwner();
+        }
+        _;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+    constructor(address entryPoint) Ownable(msg.sender) {
+        i_entryPoint = IEntryPoint(entryPoint);
+    }
+
+    receive() external payable { }
+
+    /*//////////////////////////////////////////////////////////////
+                           EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function execute(address dest, uint256 value, bytes calldata functionData) external requireFromEntryPointOrOwner {
+        (bool success, bytes memory result) = dest.call{ value: value }(functionData);
+        if (!success) revert MinimalAccount__CallFailed(result);
+    }
 
     // A signature is valid, if it's the MinimalAccount owner
     function validateUserOp(
@@ -17,10 +63,17 @@ contract MinimalAccount is IAccount, Ownable {
         uint256 missingAccountFunds
     )
         external
+        requireFromEntryPoint
         returns (uint256 validationData)
     {
         _validateSignature(userOp, userOpHash);
+        _validateNonce(userOp);
+        _payPrefund(missingAccountFunds);
     }
+
+    /*//////////////////////////////////////////////////////////////
+                           INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
     function _validateSignature(
         PackedUserOperation calldata userOp,
@@ -31,5 +84,35 @@ contract MinimalAccount is IAccount, Ownable {
         returns (uint256 validationData)
     {
         bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+
+        if (signer != owner()) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return SIG_VALIDATION_SUCCESS;
+    }
+
+    function _validateNonce(PackedUserOperation calldata userOp) internal {
+        if (userOp.nonce != ourNonce) revert NonceMismatch();
+        ourNonce++;
+    }
+
+    function _payPrefund(uint256 missingAccountFunds) internal {
+        if (missingAccountFunds != 0) {
+            (bool success,) = payable(msg.sender).call{ value: missingAccountFunds, gas: type(uint256).max }("");
+            (success);
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                GETTERS
+    //////////////////////////////////////////////////////////////*/
+
+    function getEntryPoint() external view returns (address) {
+        return address(i_entryPoint);
+    }
+
+    function getNonce() external view returns (uint256) {
+        return ourNonce;
     }
 }
